ab3ff35a99f6665693e04a55a521f4d63c149d02
quin210 <vothinhuquynh0210200@gmail.com>
2024-03-17 05:23:15 +0700

    Dine bai 5

---
+++ BaiBuoi2/Bai4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		Console.WriteLine("Enter the numeber: ");
+		int n = int.Parse(Console.ReadLine());
+		int ans = 1,  i = 1 ;
+		do
+		{
+			ans *= n;
+			n--;
+		}
+		while (n > 0);
+
+		Console.WriteLine($"Answer is {ans}.");
+		Cosole.ReadKey();
+	}
+}
---
+++ BaiBuoi2/Bai5.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        var rand = new Random();
+        Console.WriteLine("One random integer:");
+        int ramNum = rand.Next(1, 100);
+        Console.WriteLine(ramNum);
+
+        Console.WriteLine("Enter your number: ");
+        int num = int.Parse(Console.ReadLine());
+
+        if (num == ramNum) Console.WriteLine("The same numeber!!");
+        else Console.WriteLine("It's wrong.");
+    }
+}
