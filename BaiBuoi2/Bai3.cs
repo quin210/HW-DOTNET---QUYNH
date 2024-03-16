8c769fca33b294bce38326af252ab8cb5cf5f7e7
quin210 <vothinhuquynh0210200@gmail.com>
2024-03-17 05:07:32 +0700

    done bai 3

--- BaiBuoi2/Bai3.cs
+++ BaiBuoi2/Bai3.cs
@@ -4,5 +4,24 @@ public class Class1
 {
 	public Class1()
 	{
+		string chuoiKyTu; 
+		chuoiKyTu = Console.ReadLine();
+		char[1000] check = {0};
+		for(int i = 0; i < chuoiKyTu.Lenght(); i++)
+		{
+			if (check[chuoiKyTu[i]] == 1)
+			{
+				continue;
+			}
+			else
+			{
+				check[chuoiKyTu[i]] = 1;
+				int count = 0;
+                for (int j = i + 1; j < chuoiKyTu.Length; j++)
+                {
+                    if (chuoiKyTu[j] == chuoiKyTu[i]) count++;
+                }
+            }
+		}
 	}
 }
