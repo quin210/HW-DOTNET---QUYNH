144bc7e514ae3484dc5c8d63228c0027055deede
quin210 <vothinhuquynh0210200@gmail.com>
2024-03-17 05:34:17 +0700

    Done all

---
+++ BaiBuoi2/Bai6.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		Console.WriteLine("Enter your password: ");
+		string passWord = Console.ReadLine();
+		int PwdLenght = passWord.Length;
+		var check1 = 0;
+		if (PwdLenght >= 6 && PwdLenght <= 12)
+			check1 = 1;
+		int i = PwdLenght - 1;
+        do
+		{
+			if (passWord[i] == '@')
+				break;
+			i--;
+		}
+		while (i >= 0);
+	}
+}
